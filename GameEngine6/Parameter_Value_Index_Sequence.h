e815b031aec8005333cf2b29fbc4b0cfedda2c95
ゆう <6huran@gmail.com>
2023-09-01 20:51:51 +0900

    pc移動

--- GameEngine6/Parameter_Value_Index_Sequence.h
+++ GameEngine6/Parameter_Value_Index_Sequence.h
@@ -1,1 +1,21 @@
 #pragma once
+
+template<auto ...t_Parameters>
+struct S_Parameter_Value;
+
+template<size_t _Index>
+struct IS_Index_Sequence
+{
+	template<class T_Index_Sequence>
+	struct S_Index_Sequence;
+	
+
+	template<int ..._Index>
+	struct S_Index_Sequence<std::index_sequence<_Index...>>
+	{
+		using Type = S_Parameter_Value<_Index...>;
+	};
+
+	using Type = S_Index_Sequence<std::make_index_sequence<_Index>>;
+
+};
