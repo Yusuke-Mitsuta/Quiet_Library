a4b7d859f468334e40f7ceb8baf4c937faf1fb53
Yusuke-Mitsuta <103090675+Yusuke-Mitsuta@users.noreply.github.com>
2023-09-11 01:29:00 +0900

    Sortエラーの原因特定、

--- GameEngine6/Parameter_Value_Index_Sequence.h
+++ GameEngine6/Parameter_Value_Index_Sequence.h
@@ -3,7 +3,7 @@
 template<auto ...t_Parameters>
 struct S_Parameter_Value;
 
-template<size_t _Index, size_t t_Start_Number = 0>
+template<size_t _Index>
 struct IS_Index_Sequence
 {
 	template<class T_Index_Sequence>
@@ -12,7 +12,7 @@ struct IS_Index_Sequence
 	template<int ..._Index>
 	struct S_Index_Sequence<std::index_sequence<_Index...>>
 	{
-		using Type = S_Parameter_Value<t_Start_Number + _Index...>;
+		using Type = S_Parameter_Value<_Index...>;
 	};
 
 	using Type = typename S_Index_Sequence<std::make_index_sequence<_Index>>::Type;
