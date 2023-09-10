cfadb59ec7ddcccbe8a007258f233d1fbf63b083
Yusuke-Mitsuta <103090675+Yusuke-Mitsuta@users.noreply.github.com>
2023-09-11 01:07:25 +0900

    仮保存

--- GameEngine6/Parameter_Value_Index_Sequence.h
+++ GameEngine6/Parameter_Value_Index_Sequence.h
@@ -3,17 +3,16 @@
 template<auto ...t_Parameters>
 struct S_Parameter_Value;
 
-template<size_t _Index>
+template<size_t _Index, size_t t_Start_Number = 0>
 struct IS_Index_Sequence
 {
 	template<class T_Index_Sequence>
 	struct S_Index_Sequence;
-	
 
 	template<int ..._Index>
 	struct S_Index_Sequence<std::index_sequence<_Index...>>
 	{
-		using Type = S_Parameter_Value<_Index...>;
+		using Type = S_Parameter_Value<t_Start_Number + _Index...>;
 	};
 
 	using Type = typename S_Index_Sequence<std::make_index_sequence<_Index>>::Type;
