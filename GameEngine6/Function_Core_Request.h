bb5e0d8bb2b36ae79130c66489afdca6c3e55ec7
Yusuke-Mitsuta <103090675+Yusuke-Mitsuta@users.noreply.github.com>
2023-12-13 00:19:37 +0900

    Request周りの改良から

--- GameEngine6/Function_Core_Request.h
+++ GameEngine6/Function_Core_Request.h
@@ -5,6 +5,9 @@
 namespace N_Function
 {
 
+	template<class ...T_Parts>
+	struct Function_Core;
+
 	template<class T_Request_Args = invalid_t, class T_Request_Pointer = invalid_t>
 	struct Request_Core
 	{
@@ -13,6 +16,46 @@ namespace N_Function
 		using request_pointer = T_Request_Pointer;
 	};
 
+
+	//仕様
+	//供給する引数を[T_Fn_Data]から取得し、要求する引数と判定する
+	//
+	//補足
+	//関数が纏められている場合、[tuple_t]で纏める
+	template<class T_Request_Args , class T_Bind_Args>
+	struct S_Request_args
+	{
+		using type = I_Function_Args_Chack<T_Request_Args, T_Bind_Args>::request_args;
+	};
+
+	template<class T_Request_Args>
+	struct S_Request_args<T_Request_Args, invalid_t>
+	{
+		using type = T_Request_Args;
+	};
+
+	//仕様
+	//要求するポインターの型を[T_Fn_Data]から取得し、
+	// 互換性のあるポインターがセットされて入れば要求を取り消す
+	//
+	//補足
+	//関数が纏められている場合、[tuple_t]で纏める
+	//必要でない場合、[invalid_t]が返る
+	template<class T_Request_pointer ,class T_Pointer ,
+		bool t_judge = convertible_to<T_Pointer, T_Request_pointer>>
+		struct S_Request_pointer
+	{
+		using type = T_Request_pointer;
+	};
+
+	template<class T_Request_pointer, class T_Pointer>
+	struct S_Request_pointer<T_Request_pointer, T_Pointer, 1>
+	{
+		using type = invalid_t;
+	};
+
+
+
 	template<class T_Fn_Data>
 	struct I_Request
 	{
@@ -20,50 +63,9 @@ namespace N_Function
 		template<class T_Request>
 		struct I_Request_Data
 		{
-			//仕様
-			//供給する引数を[T_Fn_Data]から取得し、要求する引数と判定する
-			//
-			//補足
-			//関数が纏められている場合、[tuple_t]で纏める
-			template<class T_Request_Args = typename T_Request::request_args,
-				class T_Bind_Args = typename T_Fn_Data::bind_args>
-			struct S_Request_args
-			{
-				using type = I_Function_Args_Chack<T_Request_Args, T_Bind_Args>::request_args;
-			};
-
-			template<class T_Request_Args>
-			struct S_Request_args<T_Request_Args, invalid_t>
-			{
-				using type = T_Request_Args;
-			};
-
-
-			//仕様
-			//要求するポインターの型を[T_Fn_Data]から取得し、
-			// 互換性のあるポインターがセットされて入れば要求を取り消す
-			//
-			//補足
-			//関数が纏められている場合、[tuple_t]で纏める
-			//必要でない場合、[invalid_t]が返る
-			template<class T_Request_pointer = typename T_Request::request_pointer,
-				class T_Pointer = typename T_Fn_Data::pointer,
-				bool t_judge = convertible_to<T_Pointer, T_Request_pointer>>
-				struct S_Request_pointer
-			{
-				using type = T_Request_pointer;
-			};
-
-			template<class T_Request_pointer, class T_Pointer>
-			struct S_Request_pointer<T_Request_pointer, T_Pointer, 1>
-			{
-				using type = invalid_t;
-			};
-
 			using type = Request_Core<
-				typename S_Request_args<>::type,
-				typename S_Request_pointer<>::type>;
-
+				typename S_Request_args<typename T_Request::request_args, typename T_Fn_Data::bind_args>::type,
+				typename S_Request_pointer<typename T_Request::request_pointer, typename T_Fn_Data::pointer>::type>;
 		};
 
 		template<class T_Request>
@@ -78,7 +80,7 @@ namespace N_Function
 			using type = tuple_t<typename S_Request<T_Request>::type...>;
 		};
 
-		template<class T_Fn = typename T_Fn_Data::function>
+		template<class T_Fn = T_Fn_Data>
 		struct S_Request_Access
 		{
 			using type = T_Fn::request;
@@ -97,7 +99,6 @@ namespace N_Function
 		};
 
 		using type = S_Request_Access<>::type;
-
 	};
 
 }
