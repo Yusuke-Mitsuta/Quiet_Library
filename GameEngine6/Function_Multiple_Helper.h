801c597ccad60443b8a06c583c7200626dacd815
Yusuke-Mitsuta <103090675+Yusuke-Mitsuta@users.noreply.github.com>
2023-12-25 20:38:40 +0900

    conceptの名前変更漏れのエラー対処 I_Function_Multiple_Helperでポインターの判定が正常に行えてなかった。
    requires節、requires式の仕様を解読
    
    requires式：requiresが掛かっているテンプレートで取得する型以外の型を判定に使用する場合、
    requires式から見て型が確定していないといけない。
    
    例：アウタークラスのusing宣言を用いる場合、
    template<class T>
    struct Hoge
    {
        using type =int ; ○
        using type = T ; ×
    }
    となる。
    
    requires節：requires節が掛かっているテンプレートで取得する型以外も判定に仕様できる

--- GameEngine6/Function_Multiple_Helper.h
+++ GameEngine6/Function_Multiple_Helper.h
@@ -8,7 +8,10 @@ namespace N_Function
 {
 	template<class ...T_Parts>
 	struct Function_Core;
-	
+
+	template<class ...T_Parts>
+	struct I_Function_Superficial_Data;
+
 	//仕様
 	//関数に対して、次の関数オブジェクトが出てくるまでの間の型が引数の型と互換性があるか判定する
 	//メソッドに対しては、上記に加え、メソッドの前の型がポインターの型と互換性があるか判定し、
@@ -32,8 +35,8 @@ namespace N_Function
 	struct I_Function_Multiple_Helper
 	{
 	private:
-		
-		using front_t = std::tuple_element_t<0, tuple_t<T_Fn_Parts...>>;
+
+		using front_t =typename tuple_t<T_Fn_Parts...>::type;
 
 		template<class T_Tuple, class T_method = typename Function_Core<typename T_Tuple::type>::function>
 		struct S_Method_Search
@@ -55,16 +58,25 @@ namespace N_Function
 		};
 
 
-		template<class T_Tuple_Method_Bound,class T_Tuple_Access_Number>
-		struct S_Result;
+		//仕様
+		//結果を返すクラス
+		//
+		//テンプレート
+		//[T_Tuple_Method_Bound]::[Function_Core]が纏められた[tuple_t]
+		//[T_Tuple_Access_Number]::纏められた関数オブジェクト一つに対して、使用した型の番号をが纏められた[tupel_t]
+		template<class T_Tuple_Method_Bound, class T_Tuple_Access_Number>
+		struct S_Result
+		{
+			using type = T_Tuple_Method_Bound;
 
-		template<class T_Tuple,
-			class T_Tuple_Method_Bound = tuple_t<>,
-			class T_Tuple_Access_Number = tuple_t<>>
+			using access_number = T_Tuple_Access_Number;
+
+		};
+
+
+		template<class T_Tuple,class T_Tuple_Method_Bound = tuple_t<>,class T_Tuple_Access_Number = tuple_t<>>
 		struct S_Method_Bound
 		{
-
-
 			template<class T_Method_Point = typename S_Method_Search<T_Tuple>::type>
 			struct S_Method_Point
 			{
@@ -75,16 +87,16 @@ namespace N_Function
 
 				template<class T_Method_Point>
 				using access_numbers =
-					N_Tuple::U_range_index_sequence<T_Method_Point::tail_size, T_Tuple::tail_size+1>;
-				
-				template<class T_Tuple,class T_Method,class T_access_numbers>
-				using Method_Bound =typename S_Method_Bound<typename T_Tuple::next,
-					N_Tuple::U_Insert<T_Tuple_Method_Bound,typename T_Method::type>,
-					N_Tuple::U_Insert<T_Tuple_Access_Number,T_access_numbers>>::type;
+					N_Tuple::U_range_index_sequence<T_Method_Point::tail_size, T_Tuple::tail_size + 1>;
+
+				template<class T_Tuple, class T_Method, class T_access_numbers>
+				using Method_Bound = typename S_Method_Bound<typename T_Tuple::next,
+					N_Tuple::U_Insert<T_Tuple_Method_Bound, typename T_Method::type>,
+					N_Tuple::U_Insert<T_Tuple_Access_Number, T_access_numbers>>::type;
 
 				template<class T_Tuple>
-				using Function_Data = 
-					typename N_Tuple::I_Expand_Set<Function_Core,T_Tuple>::type;
+				using Function_Data =
+					typename N_Tuple::I_Expand_Set<I_Function_Superficial_Data, T_Tuple>::type;
 
 
 				//仕様
@@ -100,36 +112,42 @@ namespace N_Function
 					class T_Commond_Point_Check = Function_Data<N_Tuple::U_Insert<typename chack_Data<T_Method_Point>::front, front_t>>>
 				struct S_Pointer_Chack
 				{
-					using type = 
+					using type =
 						Method_Bound<T_Method_Point,
 						T_Function, access_numbers<T_Method_Point>>;
 				};
 
 				//仕様
 				//専用で設定されたポインターを判定する
-				template<class ...T_Parts,class T_Dedicated_Point_Check, class T_Commond_Point_Check>
-					requires ((std::is_pointer_v<std::remove_reference_t<typename T_Method_Point::next_t>>) &&
-					(not_same_as<typename  Function_Core<T_Parts...>::request_pointer, typename T_Dedicated_Point_Check::request_pointer>))
-				struct S_Pointer_Chack<Function_Core<T_Parts...>,T_Dedicated_Point_Check, T_Commond_Point_Check>
+				template<class T_Function, class T_Dedicated_Point_Check, class T_Commond_Point_Check>
+					requires is_pointer<std::remove_reference_t<typename T_Method_Point::next_t>> &&
+					requires
+				{
+					requires same_as_not<typename T_Function::request_pointer, typename T_Dedicated_Point_Check::request_pointer>;
+				}
+				struct S_Pointer_Chack<T_Function, T_Dedicated_Point_Check, T_Commond_Point_Check>
 				{
 					using type =
 						Method_Bound<typename T_Method_Point::next,
 						T_Dedicated_Point_Check,
 						access_numbers<typename T_Method_Point::next>>;
-
 				};
 
 				//仕様
 				//共通で設定されたポインターを判定する
-					template<class ...T_Parts, class T_Dedicated_Point_Check, class T_Commond_Point_Check>
-						requires ((std::is_pointer_v<std::remove_reference_t<front_t>>) &&
-					((is_invalid<typename T_Dedicated_Point_Check::function>) ||
-					 (same_as<typename  Function_Core<T_Parts...>::request_pointer, typename T_Dedicated_Point_Check::request_pointer>)) &&
-				 (not_same_as<typename Function_Core<T_Parts...>::request_pointer, typename T_Commond_Point_Check::request_pointer>))
-				struct S_Pointer_Chack<Function_Core<T_Parts...>, T_Dedicated_Point_Check ,T_Commond_Point_Check>
+				template<class T_Function, class T_Dedicated_Point_Check, class T_Commond_Point_Check>
+					requires is_pointer<std::remove_reference_t<front_t>> &&
+					requires
+				{
+					requires is_invalid<typename T_Dedicated_Point_Check::function> ||
+					 same_as<typename T_Function::request_pointer, typename T_Dedicated_Point_Check::request_pointer>;
+
+					requires same_as_not<typename T_Function::request_pointer, typename T_Commond_Point_Check::request_pointer>;
+				}
+				struct S_Pointer_Chack<T_Function, T_Dedicated_Point_Check, T_Commond_Point_Check>
 				{
 					using type = Method_Bound<T_Method_Point,
-						T_Commond_Point_Check, 
+						T_Commond_Point_Check,
 						access_numbers<T_Method_Point>>;
 				};
 
@@ -138,7 +156,7 @@ namespace N_Function
 				// 
 				// テンプレート
 				//[T_Function_Check]:指定された引数の型を受け取るか判定する
-				template<class T_Args_chack =typename Function_Data<chack_Data<T_Method_Point>>::request_args>
+				template<class T_Args_chack = typename Function_Data<chack_Data<T_Method_Point>>::request_args>
 				struct S_Callable_Check
 				{
 					using type = S_Pointer_Chack<>::type;
@@ -153,46 +171,30 @@ namespace N_Function
 					using type = S_Method_Point<typename S_Method_Search<typename T_Method_Point::next>::type>::type;
 				};
 
-
 				using type = S_Callable_Check<>::type;
-
 			};
 
 			//仕様
 			//関数に対して、引数の型が不一致であり、全ての探索が終わった場合
 			template<class T_Head>
-			struct S_Method_Point<tuple_tp<T_Head,invalid_t,tuple_t<>>>
+			struct S_Method_Point<tuple_tp<T_Head, invalid_t, tuple_t<>>>
 			{
 				using type = S_Result<invalid_t, invalid_t>;
+
 			};
 
 			using type = S_Method_Point<>::type;
-
 		};
 
 
 		//仕様
 		//全ての型の探査が正常に終了した場合、結果を出力する
-		template<class T_Head,class T_Tail,class T_Fns, class T_Tuple_Access_Number>
-		struct S_Method_Bound<tuple_tp<T_Head,invalid_t,T_Tail>,T_Fns, T_Tuple_Access_Number>
+		template<class T_Head, class T_Tail, class T_Fns, class T_Tuple_Access_Number>
+		struct S_Method_Bound<tuple_tp<T_Head, invalid_t, T_Tail>, T_Fns, T_Tuple_Access_Number>
 		{
 			using type = S_Result<T_Fns, T_Tuple_Access_Number>;
 		};
 
-		//仕様
-		//結果を返すクラス
-		//
-		//テンプレート
-		//[T_Tuple_Method_Bound]::[Function_Core]が纏められた[tuple_t]
-		//[T_Tuple_Access_Number]::纏められた関数オブジェクト一つに対して、使用した型の番号をが纏められた[tupel_t]
-		template<class T_Tuple_Method_Bound, class T_Tuple_Access_Number>
-		struct S_Result
-		{
-			using type = T_Tuple_Method_Bound;
-
-			using access_number = T_Tuple_Access_Number;
-
-		};
 
 		using start_tuple = typename tuple_t<T_Fn_Parts...>::reverse::front;
 
